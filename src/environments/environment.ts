92b34e6f2342ccca9d4a1d2365c7c62fb1e6f2c1
Muiz Nadeem <32574315+MuizNadeem@users.noreply.github.com>
2020-08-21 18:43:34 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyAY_ZWpDR6l8JEfAW9mwg8Qf6IoK-o_Jmg",
+    apiKey: "",
       authDomain: "testbed-material.firebaseapp.com",
       databaseURL: "https://testbed-material.firebaseio.com",
-      projectId: "testbed-material",
-      storageBucket: "testbed-material.appspot.com",
-      messagingSenderId: "1069895088892"
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: ""
   }
   
 };
